07eaeade73e69e4c77521c9f0d9042005d32fa5b
d-mv <d.dash.mv@gmail.com>
2019-09-20 11:31:20 +0300

    Bugfix

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,12 @@ import * as serviceWorker from './lib/service_worker';
 import pip from 'public-ip';
 import App from './app/App';
 
-//(async () => {
-//const ip = await pip.v4();
-//fetch(`https://home-reporter-app.herokuapp.com/?ip=${ip}&time=${new Date()}`)
-//.then()
-//.catch(e => console.log(e));
-//})();
+(async () => {
+	const ip = await pip.v4();
+	fetch(`https://home-reporter-app.herokuapp.com/?ip=${ip}&time=${new Date()}`)
+		.then()
+		.catch(e => console.log(e));
+})();
 
 var mountNode = document.getElementById('app');
 ReactDOM.render(<App />, mountNode);
