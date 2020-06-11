a52811a60066a99927b074fda34c9ecbe883b490
d-mv <d.dash.mv@gmail.com>
2020-06-11 17:11:32 +0300

    fix: reporter cors

--- src/tools/report.ts
+++ src/tools/report.ts
@@ -6,15 +6,16 @@ function report(ip: string): void {
 				domain: 'visit',
 				data: {
 					ip,
-          date: new Date(),
-          to: 'homepage'
+					date: new Date(),
+					to: 'homepage'
 				}
 			}),
 			method: 'POST',
 			headers: {
 				'content-type': 'application/json',
 				'access-control-allow-origin': '*'
-			}
+			},
+			mode: 'cors'
 		};
 		fetch('https://report-srvr.herokuapp.com/report', config);
 	} catch (err) {}
